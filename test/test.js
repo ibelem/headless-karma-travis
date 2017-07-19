f965c439f5dfacd38b058d0b8f64ac3473c916aa
Belem Zhang <belem.zhang@intel.com>
2017-07-19 16:50:59 +0800

    Fix Assert Error issue in .html page
    var expect = chai.expect;
    var assert = chai.assert;

--- test/test.js
+++ test/test.js
@@ -1,9 +1,18 @@
-// const assert = chai.assert;
+var expect = chai.expect;
+var assert = chai.assert;
 
 describe('Array', () => {
   describe('#indexOf()', () => {
     it('should return -1 when the value is not present', () => {
       assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
+
+describe('Array', () => {
+  describe('#indexOf()', () => {
+    it('should return 3 when the value is present', () => {
+      assert.equal(3, [1,2,3,4].indexOf(4));
     });
   });
 });
